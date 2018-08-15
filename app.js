1bbbf823800bb8e8e66a2607ebcb45974b57bd10
The Happy Spirit <dave@happyspiritgames.com>
2018-08-15 15:34:13 -0700

    Enable CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const admin = require('./controllers/adminController')
 const library = require('./controllers/libraryController')
 const reader = require('./controllers/readerController')
 
 const app = express()
+app.use(cors())
 
 app.get('/ping', admin.ping)
 app.get('/stories', library.searchStories)
